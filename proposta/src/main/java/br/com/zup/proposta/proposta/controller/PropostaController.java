00ea708e730695d0e085ad8c331dd7a29de1418b
joaldojunior <joaldo.junior@ucsal.edu.br>
2021-07-16 11:39:12 -0300

    ref

--- proposta/src/main/java/br/com/zup/proposta/proposta/controller/PropostaController.java
+++ proposta/src/main/java/br/com/zup/proposta/proposta/controller/PropostaController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import br.com.zup.proposta.proposta.dto.PropostaDTO;
 import br.com.zup.proposta.proposta.model.Proposta;
 import br.com.zup.proposta.proposta.repository.PropostaRepository;
@@ -20,27 +23,30 @@ import br.com.zup.proposta.proposta.repository.PropostaRepository;
 @RequestMapping(value = "/proposta")
 public class PropostaController {
 
+	private final Logger logger = LoggerFactory.getLogger(Proposta.class);
+
 	private final PropostaRepository propostaRepository;
 
 	public PropostaController(PropostaRepository propostaRepository) {
 		super();
 		this.propostaRepository = propostaRepository;
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<Proposta> inserir(@RequestBody @Valid PropostaDTO dto, UriComponentsBuilder builder){
+	public ResponseEntity<Proposta> inserir(@RequestBody @Valid PropostaDTO dto, UriComponentsBuilder builder) {
 		Proposta proposta = dto.toModel();
-		
-		if(verificarDocumento(dto.getDocumento())) {
+
+		if (verificarDocumento(dto.getDocumento())) {
+			logger.error("Erro ao inserir proposta com o CPF/CNPJ={}", dto.getDocumento());
 			return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
 		}
-		
+
 		propostaRepository.save(proposta);
-		
+		logger.info("Cadastrada com sucesso a proposta com o CPF/CNPJ={}", proposta.getDocumento());
 		URI uri = builder.path("/proposta/{id}").build(proposta.getId());
 		return ResponseEntity.created(uri).build();
 	}
-	
+
 	private boolean verificarDocumento(String documento) {
 		return propostaRepository.findByDocumento(documento).isPresent();
 	}
--- proposta/src/main/java/br/com/zup/proposta/proposta/model/Proposta.java
+++ proposta/src/main/java/br/com/zup/proposta/proposta/model/Proposta.java
@@ -58,4 +58,8 @@ public class Proposta {
 		return id;
 	}
 
+	public String getDocumento() {
+		return documento;
+	}
+	
 }
