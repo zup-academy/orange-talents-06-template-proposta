7dc8c8223fa6f3f1c47a016a127e5c4593695761
joaldo.junior <joaldo.junior@zup.com.br>
2021-07-22 10:21:56 -0300

    bloqueio cartao

--- proposta/src/main/java/br/com/zup/proposta/proposta/config/SecurityConfiguration.java
+++ proposta/src/main/java/br/com/zup/proposta/proposta/config/SecurityConfiguration.java
@@ -2,37 +2,23 @@ package br.com.zup.proposta.proposta.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(auth);
-	}
-	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests(authorizeRequests -> authorizeRequests
-				.antMatchers(HttpMethod.GET, "/actuator/*").permitAll()
-				.antMatchers(HttpMethod.GET, "/proposta/").hasAuthority("SCOPE_proposta:read")
-				.antMatchers(HttpMethod.POST, "/proposta/").hasAuthority("SCOPE_proposta:write")
-				.antMatchers(HttpMethod.POST, "/biometrias").hasAuthority("SCOPE_biometria:write")
-				.antMatchers(HttpMethod.POST, "/api/cartoes/{id}/biometrias/").hasAuthority("SCOPE_biometria:write")
-				.antMatchers(HttpMethod.POST, "/api/cartoes/{id}/bloqueios/").hasAuthority("SCOPE_proposta:write")
-				.anyRequest().authenticated()
-				).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+				.antMatchers(HttpMethod.GET, "/actuator/*").permitAll().antMatchers(HttpMethod.GET, "/proposta/")
+				.hasAuthority("SCOPE_proposta:read").antMatchers(HttpMethod.POST, "/proposta/")
+				.hasAuthority("SCOPE_proposta:write").antMatchers(HttpMethod.POST, "/biometrias")
+				.hasAuthority("SCOPE_biometria:write").antMatchers(HttpMethod.POST, "/api/cartoes/{id}/biometrias/")
+				.hasAuthority("SCOPE_biometria:write").antMatchers(HttpMethod.POST, "/api/cartoes/{id}/bloqueios/")
+				.hasAuthority("SCOPE_proposta:write").anyRequest().authenticated())
+				.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
 	}
-	
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(web);
-	}
+
 }
